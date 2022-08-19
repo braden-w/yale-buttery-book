b3d4b119609bb9d5750021382ec35e619278e42c
Braden Wong <bmw02002turbo@gmail.com>
2022-08-18 19:54:40 -0700

    Feat: Working Suggest Modal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -32,6 +32,75 @@
         <q-btn v-else round flat icon="download" @click="installDialog.open" /> -->
         <q-space />
         <InstallDialog />
+        <q-dialog v-model="reportOpen" seamless position="bottom">
+          <q-card style="width: 100%">
+            <q-card-section>
+              <div class="row">
+                <div class="text-h6">Report an Issue</div>
+                <q-space></q-space>
+                <q-btn
+                  round
+                  flat
+                  icon="close"
+                  @click="closeReportDialog"
+                ></q-btn>
+              </div>
+              <div class="text-caption">
+                We'll do our best to respond within the night! This form emails
+                us at
+                <a
+                  style="color: inherit"
+                  href="mailto:yalebutterybook@gmail.com"
+                >
+                  yalebutterybook@gmail.com
+                </a>
+              </div>
+            </q-card-section>
+            <q-separator />
+            <q-form @submit="submitReportDialog()">
+              <q-card-section>
+                <q-select
+                  v-model="reportCollege"
+                  :options="residentialColleges"
+                  label="Topic"
+                >
+                </q-select>
+                <q-input v-model="reportMessage" label="Message" />
+                <q-input
+                  v-model="reportContact"
+                  placeholder="Email, phone, etc. for us to reach out!"
+                  stack-label
+                  label="Contact Info"
+                  lazy-rules
+                  :rules="[
+                    (val) =>
+                      (val !== null &&
+                        val !== '' &&
+                        isValidEmailOrPhone(val)) ||
+                      'Please enter a valid email or phone number so we can get back to you!!',
+                  ]"
+                />
+              </q-card-section>
+              <q-card-actions>
+                <q-space></q-space>
+                <q-btn
+                  label="Close"
+                  padding="xs lg"
+                  flat
+                  @click="closeReportDialog()"
+                >
+                </q-btn>
+                <q-btn
+                  label="Submit"
+                  color="accent"
+                  padding="xs lg"
+                  type="submit"
+                >
+                </q-btn>
+              </q-card-actions>
+            </q-form>
+          </q-card>
+        </q-dialog>
         <q-btn round flat icon="info" to="/about" />
         <!-- <q-btn
           v-if="$q.screen.lt.sm"
@@ -124,9 +193,13 @@
 
 <script setup lang="ts">
 // import EssentialLink from 'components/EssentialLink.vue';
+import { createClient } from '@supabase/supabase-js';
+import axios from 'axios';
+import { useQuasar } from 'quasar';
 import InstallDialog from 'src/components/InstallDialog.vue';
 // import { ref } from 'vue';
 import { residentialColleges } from 'src/shared/butteries';
+import { ref } from 'vue';
 
 // const linksList = [
 //   {
@@ -151,8 +224,119 @@ import { residentialColleges } from 'src/shared/butteries';
 
 // const leftDrawerOpen = ref(false);
 
+const reportOpen = ref(false);
+const reportCollege = ref('');
+const reportMessage = ref('');
+const reportContact = ref('');
+function openReportDialog() {
+  reportOpen.value = true;
+}
+
+function closeReportDialog() {
+  reportOpen.value = false;
+}
+
+function setReportCollege(college: string) {
+  reportCollege.value = college;
+}
+
+function setReportMessage(message: string) {
+  reportMessage.value = message;
+}
+
+const $q = useQuasar();
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+function isEmail(email: string) {
+  const isEmailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return isEmailRegex.test(email);
+}
+
+async function submitReportDialog() {
+  const report_date = new Date().toDateString();
+  const report_time = new Date().toTimeString();
+  const loadingNotification = $q.notify({
+    message: 'Sending report...',
+    classes: 'yale-blue-1',
+    spinner: true,
+  });
+
+  const { error } = await supabase.from('reports').insert({
+    name: reportCollege.value,
+    report_message: reportMessage.value,
+    report_contact: reportContact.value,
+    report_date,
+    report_time,
+  });
+
+  const email = await axios({
+    method: 'post',
+    url: 'https://api.mailgun.net/v3/yalebutterybook.com/messages',
+    params: {
+      from: 'Yale Buttery Book <yalebutterybook@gmail.com>',
+      to: isEmail(reportContact.value)
+        ? reportContact.value
+        : 'yalebutterybook@gmail.com',
+      cc: 'braden.wong@yale.edu',
+      subject: `Yale Buttery Book Report: ${reportMessage.value}`,
+      text: `To whom it may concern,
+        
+This email confirms receipt of your report on ${report_date} at ${report_time} with the following details:
+    
+        College: ${reportCollege.value}
+        Message: ${reportMessage.value}
+        Contact: ${reportContact.value}
+
+We'll respond to you shortly.
+
+Warmly,
+Yale Buttery Book Team`,
+    },
+    auth: {
+      username: 'api',
+      password: import.meta.env.VITE_MAILGUN_API_KEY,
+    },
+  });
+  if (email.status === 200) {
+    console.log('email sent', email);
+  } else {
+    console.error('email failed', email);
+  }
+
+  loadingNotification();
+  closeReportDialog();
+  // if (error) {
+  //   $q.notify({
+  //     message: 'Error sending report',
+  //     caption: error.message,
+  //     color: 'negative',
+  //     icon: 'error'
+  //   });
+  // } else {
+  $q.notify({
+    message: 'Thank you, issue reported!',
+    caption: reportMessage.value,
+    classes: 'yale-blue-1',
+    icon: 'campaign',
+  });
+  // }
+}
+
+function isValidEmailOrPhone(text: string): boolean {
+  return (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(text) ||
+    /^\d{10}$/.test(text)
+  );
+}
+
 function reportGeneral() {
-  return null;
+  openReportDialog();
+  setReportCollege(residentialColleges[0]);
+  setReportMessage('It would be great if...');
 }
 
 // const essentialLinks = linksList;
