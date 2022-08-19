4f391cb3d9e0a16b44fe9dff11edc78bec8d4014
Braden Wong <bmw02002turbo@gmail.com>
2022-08-19 00:54:50 -0700

    Feat: Vue-3 Calendar Working

--- src/components/ButteryCardCalendar.vue
+++ src/components/ButteryCardCalendar.vue
@@ -1,13 +1,18 @@
 <template>
-  <div
+  <FullCalendar
     :id="props.buttery?.calendarID"
+    :options="calendarOptions"
     :style="`{height:${calendarHeight}}`"
-  ></div>
+  ></FullCalendar>
 </template>
 
 <script setup lang="ts">
 import { PropType } from 'vue';
 import { Buttery, TimeRemaining, TimeUntil } from 'src/shared/types-butteries';
+import '@fullcalendar/core/vdom';
+import FullCalendar from '@fullcalendar/vue3';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 const props = defineProps({
   buttery: {
@@ -17,10 +22,29 @@ const props = defineProps({
 });
 
 // --- Begin Calendar Logic ---
-import '@fullcalendar/core/vdom';
-import { Calendar } from '@fullcalendar/core';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import timeGridPlugin from '@fullcalendar/timegrid';
 
 const calendarHeight = 300;
+
+const calendarOptions = {
+  plugins: [timeGridPlugin, googleCalendarPlugin],
+  height: calendarHeight,
+  initialView: 'timeGrid3Day',
+  headerToolbar: {
+    start: 'prev,next',
+    center: '', //'title',
+    end: 'timeGridDay,timeGrid3Day,timeGridWeek',
+  },
+  scrollTime: props.buttery?.startTime,
+  views: {
+    timeGrid3Day: {
+      type: 'timeGrid',
+      duration: { days: 3 },
+      buttonText: '3 day',
+    },
+  },
+  googleCalendarApiKey: import.meta.env.VITE_GOOGLE_CALENDAR_API_KEY,
+  events: {
+    googleCalendarId: props.buttery.calendarID,
+  },
+};
 </script>
