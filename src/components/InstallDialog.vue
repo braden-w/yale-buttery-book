25754cbe7bc5194694cd76d77577d51735d41c38
Braden Wong <bmw02002turbo@gmail.com>
2022-09-16 00:25:12 -0400

    Feat: Install Dialog

--- src/components/InstallDialog.vue
+++ src/components/InstallDialog.vue
@@ -6,54 +6,24 @@
 
         <div class="text-caption">We are now published to the App Store!</div>
       </q-card-section>
-      <q-tabs
-        v-model="tab"
-        class="text-grey"
-        active-color="white"
-        indicator-color="primary"
-        inline-label
-      >
-        <q-tab name="ios" icon="apple" label="iOS" />
-        <q-tab name="android" icon="android" label="Android" />
-      </q-tabs>
       <q-separator />
+      <div class="q-pa-md q-gutter-sm">
+        <div class="row">
+          <q-space></q-space>
 
-      <q-tab-panels v-model="tab">
-        <q-tab-panel name="ios">
-          <p>We are now published to the App Store!</p>
-          <div class="row">
-            <q-space></q-space>
-            <a
-              href="https://apps.apple.com/us/app/yale-buttery-book/id1595499598"
-              target="_blank"
-            >
-              <img src="~assets/DownloadToAppStore.svg" />
-            </a>
-            <q-space> </q-space>
-          </div>
+          <a
+            href="https://apps.apple.com/us/app/yale-buttery-book/id1595499598"
+            target="_blank"
+          >
+            <img src="~assets/DownloadToAppStore.svg" />
+          </a>
 
-          <!-- <p>
-            In Safari, tap the
-            <q-icon name="ios_share" />
-            button, then 'Add to Home Screen'.
-          </p>
-
-          <q-card>
-            <q-img src="~assets/iOS_install_instructions.png">
-            </q-img>
-          </q-card> -->
-        </q-tab-panel>
-        <q-tab-panel name="android">
-          <p>
-            In Chrome, tap the <q-icon name="more_vert" /> button, then 'Install
-            App'.
-          </p>
-          <q-card>
-            <q-img src="~assets/android_install_instructions.png"> </q-img>
-          </q-card>
-          <p>Alternatively, just tap the 'Add to Home Screen' banner below.</p>
-        </q-tab-panel>
-      </q-tab-panels>
+          <q-space> </q-space>
+          <q-btn color="accent" icon="android" to="android" label="Android">
+          </q-btn>
+          <q-space></q-space>
+        </div>
+      </div>
     </q-card>
   </q-dialog>
 </template>
@@ -61,6 +31,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useDialogPluginComponent } from 'quasar';
-const tab = ref('ios');
 defineEmits([...useDialogPluginComponent.emits]);
 </script>
