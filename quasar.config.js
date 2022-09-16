f323e0caf364e5c2f9638c56e4623cd4f029193a
Braden Wong <bmw02002turbo@gmail.com>
2022-09-15 23:16:43 -0400

    Feat: AppVisibility

--- quasar.config.js
+++ quasar.config.js
@@ -114,7 +114,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Dialog', 'Notify'],
+      plugins: ['Dialog', 'Notify', 'AppVisibility'],
     },
 
     // animations: 'all', // --- includes all animations
