ae7f367e897c8c458abd1962b56de65ea361f1bd
Braden Wong <bmw02002turbo@gmail.com>
2023-01-15 16:14:47 -0500

    Feat: Simplify Buttery Card

--- src/components/ButteryCardList.vue
+++ src/components/ButteryCardList.vue
@@ -1,34 +1,32 @@
 <template>
-  <div>
-    <q-list bordered class="rounded-borders">
-      <ButteryCard
-        v-for="(buttery, index) in props.butteries"
-        :key="index"
-        :buttery="buttery"
-      />
-      <div v-if="props.butteries.length === 0">
-        <q-expansion-item>
-          <template #header>
-            <q-item>
-              <q-item-section avatar>
-                <q-avatar font-size="30px" icon="schedule" size="40px" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label overline>{{
-                  props.emptyMessage.overline
-                }}</q-item-label>
-                <q-item-label>{{ props.emptyMessage.header }}</q-item-label>
-                <q-item-label caption>
-                  {{ props.emptyMessage.text }}
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-space />
-          </template>
-        </q-expansion-item>
-      </div>
-    </q-list>
-  </div>
+  <q-list>
+    <ButteryCard
+      v-for="(buttery, index) in props.butteries"
+      :key="index"
+      :buttery="buttery"
+    />
+    <div v-if="props.butteries.length === 0">
+      <q-expansion-item>
+        <template #header>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar font-size="30px" icon="schedule" size="40px" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label overline>{{
+                props.emptyMessage.overline
+              }}</q-item-label>
+              <q-item-label>{{ props.emptyMessage.header }}</q-item-label>
+              <q-item-label caption>
+                {{ props.emptyMessage.text }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-space />
+        </template>
+      </q-expansion-item>
+    </div>
+  </q-list>
 </template>
 
 <script setup lang="ts">
