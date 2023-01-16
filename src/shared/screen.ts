c71f3407778dc44d7ce7948e69193759c970c2b9
Braden Wong <bmw02002turbo@gmail.com>
2023-01-16 16:19:05 -0500

    Fix: Width

--- src/shared/screen.ts
+++ src/shared/screen.ts
@@ -1,3 +1,3 @@
 import { Screen } from 'quasar';
-export const BREAKPOINT = 455;
-export const isMobile = Screen.width < BREAKPOINT;
+const BREAKPOINT = 455;
+export const isMobile = Screen['width'] < BREAKPOINT;
