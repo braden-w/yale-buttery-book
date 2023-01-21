e0cb56e18629d8d98c17a5badabd4e887e56ccb2
Braden Wong <bmw02002turbo@gmail.com>
2023-01-20 22:46:55 -0500

    chore: minor lines removed

--- src/shared/syncButteries.ts
+++ src/shared/syncButteries.ts
@@ -1,21 +1,13 @@
 import { formatDistance } from 'date-fns';
-import { ref, Ref } from 'vue';
 import axios from 'axios';
-import { Buttery, butteries } from './butteries';
+import { Buttery } from './butteries';
 import { GCalAPIResponse } from 'src/types/GCalAPIResponse';
-
-export const gcalButterySchedule = ref<GCalAPIResponse>();
-
-/*** List of butteries that are currently open */
-export const OpenButteryCardList: Ref<Buttery[]> = ref([]);
-/*** List of butteries that are currently closed */
-export const ClosedButteryCardList: Ref<Buttery[]> = ref([]);
 
 let scheduleSyncInterval: NodeJS.Timeout | null = null;
 let calendarSyncInterval: NodeJS.Timeout | null = null;
 
 /*** Fetch the Google Calendar schedule and save it into gcalButterySchedule */
-export async function refreshGcalButterySchedule() {
+export async function refreshGcalButterySchedule(butteries: Buttery[]) {
   const startRange = new Date(new Date().setDate(new Date().getDate() - 1));
   const endRange = new Date(new Date().setDate(new Date().getDate() + 30));
   const startRangeISO = startRange.toISOString();
@@ -27,21 +19,24 @@ export async function refreshGcalButterySchedule() {
       id: buttery.calendarID,
     })),
   };
-  const res = await axios({
+  const { data } = (await axios({
     url: `https://www.googleapis.com/calendar/v3/freeBusy?key=${
       import.meta.env.VITE_GOOGLE_CALENDAR_API_KEY
     }`,
     method: 'POST',
     data: requestBody,
-  });
-  gcalButterySchedule.value = res.data as GCalAPIResponse;
+  })) as { data: GCalAPIResponse };
+  return data.calendars;
 }
 
-function updateTimeToOpenAndClose(buttery: Buttery) {
+function updateTimeToOpenAndClose(
+  gcalButterySchedule: GCalAPIResponse,
+  buttery: Buttery
+) {
   const now = new Date();
   const butteryScheduleBusy =
     /* Creating a schedule for the buttery. */
-    gcalButterySchedule.value?.calendars[buttery.calendarID].busy;
+    gcalButterySchedule?.calendars[buttery.calendarID].busy;
   if (!butteryScheduleBusy) {
     return;
   }
@@ -80,36 +75,23 @@ function updateTimeToOpenAndClose(buttery: Buttery) {
   }
 }
 
-export function clearButteryCardListAndRefreshButteries() {
-  OpenButteryCardList.value = [];
-  ClosedButteryCardList.value = [];
-  for (const buttery of butteries) {
-    updateTimeToOpenAndClose(buttery);
-    if (buttery.isOpen) {
-      OpenButteryCardList.value.push(buttery);
-    } else {
-      ClosedButteryCardList.value.push(buttery);
-    }
-  }
-}
-
 export function startSync() {
   // Stop sync if residual
   stopSync();
   console.log('starting sync');
   (async () => {
-    await refreshGcalButterySchedule();
-    clearButteryCardListAndRefreshButteries();
+    // await refreshGcalButterySchedule();
+    // clearButteryCardListAndRefreshButteries();
   })();
 
   // Refresh GCal schedule every minute
   calendarSyncInterval = setInterval(async () => {
-    await refreshGcalButterySchedule();
+    // await refreshGcalButterySchedule();
   }, 60 * 1000);
 
   // Refresh open and closed butteries every second
   scheduleSyncInterval = setInterval(() => {
-    clearButteryCardListAndRefreshButteries();
+    // clearButteryCardListAndRefreshButteries();
   }, 1 * 1000);
   console.log('sync started', calendarSyncInterval, scheduleSyncInterval);
 }
