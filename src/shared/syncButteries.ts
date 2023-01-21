75797022a884990d3c772ebc22b1c3e1d78476b2
Braden Wong <bmw02002turbo@gmail.com>
2023-01-20 22:54:46 -0500

    chore: remove dead functions

--- src/shared/syncButteries.ts
+++ src/shared/syncButteries.ts
@@ -1,10 +1,6 @@
-import { formatDistance } from 'date-fns';
 import axios from 'axios';
 import { Buttery } from './butteries';
 import { GCalAPIResponse } from 'src/types/GCalAPIResponse';
-
-let scheduleSyncInterval: NodeJS.Timeout | null = null;
-let calendarSyncInterval: NodeJS.Timeout | null = null;
 
 /*** Fetch the Google Calendar schedule and save it into gcalButterySchedule */
 export async function refreshGcalButterySchedule(butteries: Buttery[]) {
@@ -27,92 +23,4 @@ export async function refreshGcalButterySchedule(butteries: Buttery[]) {
     data: requestBody,
   })) as { data: GCalAPIResponse };
   return data.calendars;
-}
-
-function updateTimeToOpenAndClose(
-  gcalButterySchedule: GCalAPIResponse,
-  buttery: Buttery
-) {
-  const now = new Date();
-  const butteryScheduleBusy =
-    /* Creating a schedule for the buttery. */
-    gcalButterySchedule?.calendars[buttery.calendarID].busy;
-  if (!butteryScheduleBusy) {
-    return;
-  }
-  let previousEventRange = {
-    start: new Date(butteryScheduleBusy?.[0]?.start),
-    end: new Date(butteryScheduleBusy?.[0]?.end),
-  };
-  for (const event of butteryScheduleBusy) {
-    const eventRange = {
-      start: new Date(event.start),
-      end: new Date(event.end),
-    };
-    // If buttery is open
-    if (eventRange.start <= now && eventRange.end >= now) {
-      buttery.isOpen = true;
-      const millisecondsToClose = eventRange.end.getTime() - now.getTime();
-      const humanReadable = formatDistance(millisecondsToClose, 0, {
-        addSuffix: true,
-        includeSeconds: true,
-      });
-
-      buttery.opensIn = `Closes ${humanReadable}`;
-    }
-    // If buttery is closed
-    if (eventRange.start >= now && previousEventRange.end <= now) {
-      buttery.isOpen = false;
-      const millisecondsToOpen = eventRange.start.getTime() - now.getTime();
-      const humanReadable = formatDistance(millisecondsToOpen, 0, {
-        addSuffix: true,
-        includeSeconds: true,
-      });
-
-      buttery.opensIn = `Opens ${humanReadable}`;
-    }
-    previousEventRange = eventRange;
-  }
-}
-
-export function startSync() {
-  // Stop sync if residual
-  stopSync();
-  console.log('starting sync');
-  (async () => {
-    // await refreshGcalButterySchedule();
-    // clearButteryCardListAndRefreshButteries();
-  })();
-
-  // Refresh GCal schedule every minute
-  calendarSyncInterval = setInterval(async () => {
-    // await refreshGcalButterySchedule();
-  }, 60 * 1000);
-
-  // Refresh open and closed butteries every second
-  scheduleSyncInterval = setInterval(() => {
-    // clearButteryCardListAndRefreshButteries();
-  }, 1 * 1000);
-  console.log('sync started', calendarSyncInterval, scheduleSyncInterval);
-}
-
-export function stopSync() {
-  console.log(
-    'stopping sync, current running interval timer :>>',
-    calendarSyncInterval,
-    scheduleSyncInterval
-  );
-  if (calendarSyncInterval) {
-    clearInterval(calendarSyncInterval);
-    calendarSyncInterval = null;
-  }
-  if (scheduleSyncInterval) {
-    clearInterval(scheduleSyncInterval);
-    scheduleSyncInterval = null;
-  }
-  console.log(
-    'sync stopped, current running interval timers :>>',
-    calendarSyncInterval,
-    scheduleSyncInterval
-  );
 }
