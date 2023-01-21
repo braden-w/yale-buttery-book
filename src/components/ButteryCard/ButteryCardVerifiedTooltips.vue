dc7b6eb59af24db0b152092a86aa063ff07cf443
Braden Wong <bmw02002turbo@gmail.com>
2023-01-21 17:18:01 -0500

    feat: show tooltip on tap

--- src/components/ButteryCard/ButteryCardVerifiedTooltips.vue
+++ src/components/ButteryCard/ButteryCardVerifiedTooltips.vue
@@ -1,33 +1,46 @@
 <template>
-  <q-badge v-if="props.verified === 'OPEN'" rounded color="blue">
+  <q-badge
+    v-if="props.verified === 'OPEN'"
+    rounded
+    color="blue"
+    @click.stop="showTooltip"
+  >
     <q-icon name="check" size="0.75em" />
     <q-tooltip
       class="text-body2"
       :anchor="!isMobile() ? 'center right' : undefined"
       :self="!isMobile() ? 'center left' : undefined"
       v-model="showing"
+      ref="tooltip"
     >
       Buttery Staff verifies that today is open
     </q-tooltip>
   </q-badge>
-  <q-badge v-else-if="props.verified === 'CLOSED'" rounded color="red">
+  <q-badge
+    v-else-if="props.verified === 'CLOSED'"
+    rounded
+    color="red"
+    @click.stop="showTooltip"
+  >
     <q-icon name="close" size="0.75em" />
     <q-tooltip
       class="text-body2"
       :anchor="!isMobile() ? 'center right' : undefined"
       :self="!isMobile() ? 'center left' : undefined"
       v-model="showing"
+      ref="tooltip"
     >
       Buttery Staff verifies that today is closed
     </q-tooltip>
   </q-badge>
-  <q-badge v-else rounded color="amber">
+  <q-badge v-else rounded color="amber" @click.stop="showTooltip">
     <q-icon name="" size="0.75em" />
     <q-tooltip
       class="text-body2"
       :anchor="!isMobile() ? 'center right' : undefined"
       :self="!isMobile() ? 'center left' : undefined"
       v-model="showing"
+      ref="tooltip"
     >
       Buttery Staff has not yet verified today, going by default schedule
     </q-tooltip>
@@ -38,6 +51,7 @@
 import { ref, PropType } from 'vue';
 import { Buttery } from 'src/shared/butteries';
 import { isMobile } from 'src/shared/screen';
+import { onClickOutside } from '@vueuse/core';
 
 const props = defineProps({
   verified: {
@@ -47,7 +61,10 @@ const props = defineProps({
 });
 
 const showing = ref(false);
-const toggleTooltip = () => {
-  showing.value = !showing.value;
+const showTooltip = () => (showing.value = true);
+const hideTooltip = () => {
+  if (showing.value === true) showing.value = false;
 };
+const tooltip = ref(null);
+onClickOutside(tooltip, hideTooltip);
 </script>
