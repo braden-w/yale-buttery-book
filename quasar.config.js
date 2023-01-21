9183f0012a6c1ad4ad7b0092b40eaa39a0de2722
Braden Wong <bmw02002turbo@gmail.com>
2023-01-20 20:37:59 -0500

    feat: vue query

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,11 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [ctx.mode.capacitor ? 'google-analytics' : '', 'v-viewer'],
+    boot: [
+      ctx.mode.capacitor ? 'google-analytics' : '',
+      'v-viewer',
+      'vue-query',
+    ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.css'],
