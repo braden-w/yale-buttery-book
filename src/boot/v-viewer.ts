b2265d9ae0e69860af2fa420393ee467df17706e
Braden Wong <bmw02002turbo@gmail.com>
2023-01-21 20:06:47 -0500

    chore: v-viewer uninstall

--- src/boot/v-viewer.ts
+++
@@ -1,9 +0,0 @@
-import { boot } from 'quasar/wrappers';
-import 'viewerjs/dist/viewer.css';
-import VueViewer from 'v-viewer';
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app }) => {
-  app.use(VueViewer);
-});
