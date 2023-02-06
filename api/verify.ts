19ebecfc2f0db4668efc2bae21c997d8b7fa1568
Braden Wong <bmw02002turbo@gmail.com>
2023-02-05 20:45:29 -0500

    feat: api folder

--- api/verify.ts
+++ api/verify.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { z } from 'zod';
+import { setButteryVerified } from './setButteryVerified';
 
 // Zod Types
 const idSchema = z.enum([
@@ -24,16 +25,16 @@ const valueSchema = z.enum(['OPEN', 'CLOSED'], {
   description: 'Value must be either "OPEN" or "CLOSED"',
 });
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
   try {
     const { id, value } = request.query;
-    console.log('🚀 ~ file: verify.ts:17 ~ id', id);
     const calendarId = idSchema.parse(id);
     const verifiedValue = valueSchema.parse(value);
-    return response.end(`Hello ${calendarId} ${verifiedValue}!`);
+    await setButteryVerified(calendarId, verifiedValue);
+    return response.status(200).end(`Successfully set ${id} to ${value}`);
   } catch (error) {
     return response.status(400).end(error.message);
   }
