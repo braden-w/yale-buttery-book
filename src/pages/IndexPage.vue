41a42bc67820881fba8f0d0e89e76b96b9cc45f2
Braden Wong <bmw02002turbo@gmail.com>
2023-02-21 22:20:23 -0500

    feat: just one banner

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -47,7 +47,7 @@
         </template>
       </q-banner>
 
-      <q-banner
+      <!-- <q-banner
         inline-actions
         dense
         rounded
@@ -70,7 +70,7 @@
           />
           <q-btn flat icon="close" @click="contributeBanner = false" />
         </template>
-      </q-banner>
+      </q-banner> -->
 
       <q-card flat class="q-mb-md">
         <q-card-section class="text-h5">Currently Open</q-card-section>
